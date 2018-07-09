972af6b0115f68ec9048a70e590542ab515139e9
apptest563 <40782125+apptest563@users.noreply.github.com>
2018-07-09 17:47:55 +0800

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,7 +2,6 @@ public class Rectangle {
   public int length;
   public int width;
 
-  #check comments
   public Rectangle(int length, int width) {
     this.length = length;
     this.width = width; 
